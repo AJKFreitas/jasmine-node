ecead64171ae326491af02626ef718de173a8f72
Angelo Freitas <ajkfreitas@gmail.com>
2021-12-17 17:39:56 -0300

    projeto jasmine node

---
+++ spec/jasmine_examples/exemplo.spec.js
@@ -0,0 +1,19 @@
+
+describe('Suite de Teste de Exemplo com node js', function() {
+    
+    it('Deve demostrar o uso do Jasmine com o NodeJS', function() {
+        expect(true).toEqual(true);
+    });
+        
+    
+    it('Deve demonstrar o uso do Karma em modo esculta', function() {
+        expect(true).toBe(true);
+        expect(1).toEqual(1);
+    });
+    it('Deve demonstrar o uso do Karma em modo esculta 2', function() {
+        expect(true).toBe(true);
+        expect(1).toEqual(1);
+    });
+        
+});
+    
